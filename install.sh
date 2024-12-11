688152db74c7e40b6b588359802f330fc490b8c5
Daniel Cederhammar <daniel.cederhammar@iver.se>
2024-12-11 11:18:02 +0100

    lf

--- install.sh
+++ install.sh
@@ -20,10 +20,12 @@ do
 	esac
 done
 
-# wget https://packages.microsoft.com/config/ubuntu/{echo $DISTRIB_RELEASE}/packages-microsoft-prod.deb
-# dpkg -i packages-microsoft-prod.deb
-sudo apt-get update
-sudo apt-get install -y pure-ftpd blobfuse lighttpd
+. /etc/lsb-release
+
+wget https://packages.microsoft.com/config/ubuntu/{echo $DISTRIB_RELEASE}/packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+apt-get update
+apt-get install -y pure-ftpd blobfuse lighttpd
 mkdir /ftp/
 mkdir /ftp/ftp-files
 
